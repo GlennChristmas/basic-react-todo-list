71deaa5a942d1f1988434e72bbe53846d789b9f1
Glenn Christmas <glenn_christmas@hotmail.co.uk>
2024-10-17 08:48:57 +0100

    add left-to-right prioritsation ordering of to-do items

--- src/RenderToDo.jsx
+++ src/RenderToDo.jsx
@@ -3,9 +3,15 @@ import React, { useState } from "react";
 const RenderToDo = (props) => {
   const toDoList = props.toDoList;
 
+  const priorityOrder = (arr) => {
+    return arr.sort((a, b) => b.priority - a.priority);
+  };
+
+  const prioritisedToDoList = priorityOrder(toDoList);
+
   return (
     <div className="toDoListContainer">
-      {toDoList.map((item) => {
+      {prioritisedToDoList.map((item) => {
         return (
           <div className="toDoElementGrid">
             <div>{item.task}</div>
